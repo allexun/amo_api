f984910bf53ced568f049e6559368bf8725720fa
Aleksei Kritskov <kritskov@cadmean.dev>
2023-12-07 16:38:49 +0300

    Update wip

--- src/Controller/CreateFieldController.php
+++ src/Controller/CreateFieldController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Service\AmoCustomFieldsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CreateFieldController extends AbstractController
@@ -15,9 +16,11 @@ class CreateFieldController extends AbstractController
     }
 
     #[Route('/api/field', name: 'api_field_create', methods: ['POST'])]
-    public function createField(): JsonResponse
+    public function createField(Request $request): JsonResponse
     {
-        $this->customFieldsService->createCustomFieldMultiList();
+        $parameters = json_decode($request->getContent(), true);
+
+        $this->customFieldsService->createCustomFieldMultiList($parameters['name']);
 
         return $this->json([
             'message' => 'Created',
--- src/Service/AmoCustomFieldsService.php
+++ src/Service/AmoCustomFieldsService.php
@@ -4,10 +4,15 @@ namespace App\Service;
 
 use AmoCRM\Collections\CustomFields\CustomFieldEnumsCollection;
 use AmoCRM\Collections\CustomFields\CustomFieldsCollection;
+use AmoCRM\Collections\CustomFieldsValuesCollection;
 use AmoCRM\Exceptions\AmoCRMApiException;
+use AmoCRM\Filters\PagesFilter;
 use AmoCRM\Helpers\EntityTypesInterface;
 use AmoCRM\Models\CustomFields\EnumModel;
 use AmoCRM\Models\CustomFields\MultiselectCustomFieldModel;
+use AmoCRM\Models\CustomFieldsValues\MultiselectCustomFieldValuesModel;
+use AmoCRM\Models\CustomFieldsValues\ValueCollections\MultiselectCustomFieldValueCollection;
+use AmoCRM\Models\CustomFieldsValues\ValueModels\MultiselectCustomFieldValueModel;
 
 class AmoCustomFieldsService
 {
@@ -16,30 +21,30 @@ class AmoCustomFieldsService
     ) {
     }
 
-    public function createCustomFieldMultiList(): void
+    public function createCustomFieldMultiList(string $name): void
     {
         $fields = $this->provider->getClient()->customFields(EntityTypesInterface::LEADS);
 
-        $id = random_int(0, 10000);
+        $code = 'MULTILIST'.strtoupper($name);
         $field = new MultiselectCustomFieldModel();
-        $field->setName('Example multi list '.$id);
+        $field->setName('Example multi list '.$name);
         $field->setSort(30);
-        $field->setCode('MULTILIST'.$id);
+        $field->setCode($code);
         $field->setEnums(
             (new CustomFieldEnumsCollection())
                 ->add(
                     (new EnumModel())
-                        ->setValue('Value '.random_int(1, 10000))
+                        ->setValue('Value 1')
                         ->setSort(10)
                 )
                 ->add(
                     (new EnumModel())
-                        ->setValue('Value '.random_int(1, 10000))
+                        ->setValue('Value 2')
                         ->setSort(20)
                 )
                 ->add(
                     (new EnumModel())
-                        ->setValue('Value '.random_int(1, 10000))
+                        ->setValue('Value 3')
                         ->setSort(30)
                 )
         );
@@ -49,9 +54,54 @@ class AmoCustomFieldsService
 
         try {
             $fields->add($fieldsCollection);
+            $this->updateAllCustomFieldMultiList($code, 'Value 2');
         } catch (AmoCRMApiException $e) {
             ErrorPrinter::printError($e);
             exit;
+        }
+    }
+
+    public function updateAllCustomFieldMultiList(string $code, string $value): void
+    {
+        $leads = $this->provider->getClient()->leads();
+        $page = 1;
+        $filter = new PagesFilter();
+        $filter->setLimit(100);
+        $filter->setPage($page);
+
+        while (true) {
+            try {
+                // var_dump($page);
+                $leadsCollection = $leads->get($filter);
+
+                foreach ($leadsCollection as $lead) {
+                    $leadCustomFieldsValues = new CustomFieldsValuesCollection();
+                    $customValues = new MultiselectCustomFieldValuesModel();
+                    $customValues->setFieldCode($code);
+                    $customValues->setValues(
+                        (new MultiselectCustomFieldValueCollection())
+                            ->add((new MultiselectCustomFieldValueModel())->setValue($value))
+                    );
+                    $leadCustomFieldsValues->add($customValues);
+
+                    $lead->setCustomFieldsValues($leadCustomFieldsValues);
+
+                    // var_dump($lead->getName());
+                    // var_dump($lead->getCustomFieldsValues());
+                }
+
+                $leadsCollection = $leads->update($leadsCollection);
+
+                $filter->setPage(++$page);
+
+                sleep(1);
+            } catch (AmoCRMApiException $e) {
+                if (204 === $e->getCode()) {
+                    break;
+                }
+
+                throw $e;
+            }
         }
     }
 }
